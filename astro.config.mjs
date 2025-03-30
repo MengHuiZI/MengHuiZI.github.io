0be3d592c3b37810a5f8ced4f674c54657512038
MengHuiZI <3094446052@qq.com>
2025-03-30 20:19:10 +0800

    修改文章下面为公网域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://menghuizi.pages.dev/",
+  site: "https://blog.c3ecbb8d.com/",
   base: "/",
   trailingSlash: "always",
   integrations: [
