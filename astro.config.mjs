c4321028e10cd86caefd0f02ff0f4f2c11c20f0a
MengHuiZI <3094446052@qq.com>
2025-03-30 16:52:15 +0800

    注释github链接

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://menghuizi.github.io/",
+  site: "https://menghuizi.pages.dev/",
   base: "/",
   trailingSlash: "always",
   integrations: [
